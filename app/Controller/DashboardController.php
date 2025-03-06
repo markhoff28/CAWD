ed8b5c9afb8d2f033a59963256dc610e9ebcc615
markhoff28 <Magnus-Hoffmann@web.de>
2025-03-06 14:19:12 +0100

    [CAWD-2] Move html code in separate phtml file and update controller and dashboard view for it

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -10,18 +10,21 @@ class DashboardController
   public function index()
   {
     $config = new Config('config.ini');
-    $title = $config->get('dashboard', 'title');
 
-    $widgets = [];
+    $data = [
+      'title' => $config->get('dashboard', 'title'),
+      'widgets' => []
+    ];
+
     foreach ($config->get('widgets', 'items') as $widget) {
-      $widgets[] = [
-        'icon' => $config->get($widget, 'icon'),
+      $data['widgets'][] = [
+        'icon' => html_entity_decode($config->get($widget, 'icon')),
         'url' => $config->get($widget, 'url'),
         'name' => $config->get($widget, 'name')
       ];
     }
 
     $view = new DashboardView();
-    $view->render($title, $widgets);
+    $view->render($data);
   }
 }
--- app/View/DashboardView.php
+++ app/View/DashboardView.php
@@ -4,37 +4,8 @@ namespace App\View;
 
 class DashboardView
 {
-  public function render($title, $widgets)
+  public function render($data)
   {
-?>
-    <!DOCTYPE html>
-    <html>
-
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title><?php echo htmlspecialchars($title); ?></title>
-      <link rel="stylesheet" href="public/css/style.css">
-    </head>
-
-    <body>
-      <section class="main">
-        <h1><?php echo htmlspecialchars($title); ?></h1>
-        <div class="cardBox gray" style="--i: 2;">
-          <?php foreach ($widgets as $widget): ?>
-            <div class="card">
-              <span><?php echo $widget['icon']; ?></span>
-              <a href="<?php echo htmlspecialchars($widget['url']); ?>">
-                <?php echo htmlspecialchars($widget['name']); ?>
-              </a>
-            </div>
-          <?php endforeach; ?>
-        </div>
-      </section>
-
-    </body>
-
-    </html>
-<?php
+    include __DIR__ . '/Template/dashboard.phtml';
   }
 }
